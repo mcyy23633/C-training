9247842b456a6853bff0aa36c4f3581ae1ff8099
location0422 <mcyy23633@gmail.com>
2024-01-17 00:58:59 +0800

    新增輸出格式控制的範例

---
+++ Coding/11.ppt12格式對應字範例3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+	{
+		char ch1 = 'A';
+		char ch2 = '\n';
+         
+		printf("%c\t%d", ch1 , ch1);
+		printf("%cReturn\t%d", ch2 , ch2);
+	}
+
---
+++ Coding/12.ppt13格式對應字範例4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+	char c1 = 'A';
+	char c2 = 'z';
+	printf("Char\tDec\tOct\tHex");
+	printf("\n%c\t%d\t%o\t%x", c1 , c1 , c1 , c1);
+	printf("\n%c\t%d\t%o\t%x", c2 , c2 , c2 , c2);
+}
+
