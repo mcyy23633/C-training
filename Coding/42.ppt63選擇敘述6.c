81f6d22857349c61e11783b19a9b9ba7df65ca8f
location0422 <mcyy23633@gmail.com>
2024-01-22 12:04:09 +0800

    選擇敘述練習

---
+++ Coding/42.ppt63選擇敘述6.c
@@ -0,0 +1,15 @@
+Switch (key value)
+{
+case condition value 1:
+	Section 1 Narrative;
+	break;
+case condition value 2:
+	Section 2 Narrative;
+	break;
+	¡K¡K¡K¡K..
+case condition value N:
+	Section N Description
+	break;
+default:
+default-section N statement;
+}
